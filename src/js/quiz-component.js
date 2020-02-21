345471539522c40ee1d95361567412c9d7589f4e
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-02-21 23:55:32 +0300

    Added comments

--- src/js/quiz-component.js
+++ src/js/quiz-component.js
@@ -10,18 +10,30 @@ export class QuizGame extends window.HTMLElement {
     this.startBtn = this.shadowRoot.querySelector('#start')
 
     this.responseData = {}
+    this.answerURL = 'http://vhost3.lnu.se:20080/answer/1'
     this.questionURL = 'http://vhost3.lnu.se:20080/question/1'
   }
 
+  /**
+   * Called everytime the component is added to the code
+   */
   connectedCallback () {
     this.startBtn.addEventListener('click', event => {
       this._startQuiz()
+
+      // Should be in different function...
+      // onclick.submitAnswer() or something
+      this._submitAnswer()
     })
   }
 
-  async _startQuiz (respose) {
+  /**
+   *
+   * Onclick sends first promise and parses a dom
+   */
+  _startQuiz () {
     this._removeDiv()
-    await this._getResponse(this.questionURL)
+    this._getResponse(this.questionURL)
     this._parseResponse()
   }
 
@@ -32,6 +44,9 @@ export class QuizGame extends window.HTMLElement {
     return this.responseData
   }
 
+  /**
+   * parses a .json answer and creates dom structure
+   */
   _parseResponse () {
     this._removeDiv()
     question_.innerHTML = `<p>${this.responseData.question}</p>`
@@ -41,15 +56,45 @@ export class QuizGame extends window.HTMLElement {
     this._getVal()
   }
 
+  /**
+   * POST function
+   */
+  async _submitAnswer () {
+    const data = { answer: '2' }
+    const settings = {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+
+    }
+    try {
+      const fetchResponse = await window.fetch('http://vhost3.lnu.se:20080/answer/1', settings)
+      const data = await fetchResponse.json()
+      console.log(data)
+      return data
+    } catch (error) {
+      return error
+    }
+  }
+
+  /**
+   * Gets value input field on keypdown
+   */
   _getVal () {
     const input = this.shadowRoot.querySelector('#answer')
     input.addEventListener('keydown', async event => {
       if (event.key === 'Enter') {
-        console.log(input.value)
+        return event.value
       }
     })
   }
 
+  /**
+   *  Helper function
+   *  clears div node
+   */
   _removeDiv () {
     while (this._container.firstChild) {
       this._container.removeChild(this._container.lastChild)
