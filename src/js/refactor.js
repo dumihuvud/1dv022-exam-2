60b55fe66d70533b58cf5484354b4aa1e0bbf56c
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-02-27 21:32:55 +0300

    Added question rendering and event delegation

--- src/js/refactor.js
+++ src/js/refactor.js
@@ -15,8 +15,8 @@ export class Refactor extends window.HTMLElement {
         <p class="p-inline">Start the quiz </p><button id="start" disabled>Start</button>
     </div>
     <div class="quiz">
-        <p id="question">Question</p>
-        <input type="text" id="answer" />
+        <p id="question"></p>
+        <input type="text" id="answerInput" /> <button id="answerBtn">Answer</button>
         <div class="btns">
             <button id="alt1"></button><button id="alt2"></button><button id="alt3"></button><button id="alt4"></button>
         </div>
@@ -37,8 +37,12 @@ export class Refactor extends window.HTMLElement {
         display: none;
     }
 
-    #answer {
+    #answerInput {
         display: none;
+    }
+
+    #answerBtn {
+      display: none;
     }
 
     .btns {
@@ -52,7 +56,16 @@ export class Refactor extends window.HTMLElement {
     this.constainerStart = this.shadowRoot.querySelector('.container-start')
     this.startForm = this.shadowRoot.querySelector('#username')
     this.startBtn = this.shadowRoot.querySelector('#start')
-    this.nextURL = 'http://vhost3.lnu.se:20080/question/1'
+    this.quiz = this.shadowRoot.querySelector('.quiz')
+    this.btns = this.shadowRoot.querySelector('.btns')
+    this.alt1 = this.shadowRoot.querySelector('#alt1')
+    this.alt2 = this.shadowRoot.querySelector('#alt2')
+    this.alt3 = this.shadowRoot.querySelector('#alt3')
+    this.alt4 = this.shadowRoot.querySelector('#alt4')
+    this.pQuestion = this.shadowRoot.querySelector('#question')
+    this.answerInput = this.shadowRoot.querySelector('#answerInput')
+    this.answerBtn = this.shadowRoot.querySelector('#answerBtn')
+    this.nextURL = 'http://vhost3.lnu.se:20080/question/21'
   }
 
   connectedCallback () {
@@ -61,6 +74,8 @@ export class Refactor extends window.HTMLElement {
     })
     this.startBtn.addEventListener('click', event => {
       this.hideStartForm()
+      this.getQuestion(this.nextURL)
+      this.getAnswer()
     })
   }
 
@@ -83,13 +98,35 @@ export class Refactor extends window.HTMLElement {
   }
 
   renderQuestion (obj, url) {
+    console.log(obj)
+    this.pQuestion.innerText = `${obj.question}`
+    this.quiz.style.display = 'block'
+    if (obj.alternatives) {
+      this.alt1.innerHTML = obj.alternatives.alt1
+      this.alt2.innerHTML = obj.alternatives.alt2
+      this.alt3.innerHTML = obj.alternatives.alt3
+      this.alt4.innerHTML = obj.alternatives.alt4
+      this.quiz.style.display = 'block'
+      this.btns.style.display = 'block'
+    } else {
+      this.answerInput.style.display = 'inline'
+      this.answerBtn.style.display = 'inline'
+    }
+  }
 
+  getAnswer () {
+    this.answerInput.addEventListener('input', event => {
+      console.log(this.answerInput.value)
+    })
+    this.btns.addEventListener('click', event => {
+      if (event.target.id === 'alt1' || event.target.id === 'alt2' || event.target.id === 'alt3' || event.target.id === 'alt4') {
+        console.log(event.target.id)
+      }
+    })
   }
 
   hideStartForm () {
     this.constainerStart.style.display = 'none'
-    const pTag = document.createElement('p')
-    pTag.innerText = 'Hey'
   }
 }
 
