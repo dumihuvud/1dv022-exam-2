25fb20ac4f58dbc40f032a8034a57ddd2c9c317d
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-02-28 20:31:41 +0300

    Working quiz. Need to add timer and sort score board

--- src/js/refactor.js
+++ src/js/refactor.js
@@ -6,21 +6,34 @@ export class Refactor extends window.HTMLElement {
     templateRef_.innerHTML = `
     <div class="start">
     <h4><b>The quiz game</b></h4>
-    <div id="timecontainer"></div>
-    <div class="container-start">
-        <form id="start-form">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username">
-        </form><br>
-        <p class="p-inline">Start the quiz </p><button id="start" disabled>Start</button>
-    </div>
-    <div class="quiz">
-        <p id="question"></p>
-        <input type="text" id="answerInput" /> <button id="answerBtn">Answer</button>
-        <div class="btns">
-            <button id="alt1"></button><button id="alt2"></button><button id="alt3"></button><button id="alt4"></button>
-        </div>
-    </div>
+      <div class="container-start">
+          <form id="start-form">
+              <label for="username">Username:</label>
+              <input type="text" id="username" name="username">
+          </form><br>
+          <p class="p-inline">Start the quiz </p><button id="start" disabled>Start</button>
+      </div>
+      <div class="quiz">
+          <div class="quiz-question">
+            <p id="question"></p>
+            <input type="text" id="answerInput" /> <button id="answerBtn">Answer</button>
+            <div class="btns">
+
+            </div>
+          </div>
+          <div class="response">
+            <p id="message-response"></p><button id="next-question">Next</button>
+          </div>
+      </div>
+      <div class="restart-quiz">
+        <p id="p-restart">Try again</p><button id="restart-btn">Restart</button>
+      </div>
+      <div class="score-board">
+        <p id="p-score">Score board:</p>
+        <ol id="score-list">
+        
+        </ol>
+      </div>
     </div>
 
     <style>
@@ -45,38 +58,217 @@ export class Refactor extends window.HTMLElement {
       display: none;
     }
 
+    .response {
+      display:none;
+    }
+
+    #message-response {
+      display: inline;
+    }
+
+    #next-question {
+      display: inline;
+    }
+
     .btns {
         display: none;
+    }
+
+    .restart-quiz {
+      display: none;
+    }
+
+    #p-restart {
+      display: inline;
+    }
+
+    #restart-btn {
+      display: inline;
+    }
+
+    .score-board {
+      display: none;
     }
     </style>
     `
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(templateRef_.content.cloneNode(true))
 
+    this.scoreBoard = this.shadowRoot.querySelector('.score-board')
+    this.pScore = this.shadowRoot.querySelector('#p-score')
+    this.scoreList = this.shadowRoot.querySelector('#score-list')
+
     this.constainerStart = this.shadowRoot.querySelector('.container-start')
     this.startForm = this.shadowRoot.querySelector('#username')
     this.startBtn = this.shadowRoot.querySelector('#start')
+
     this.quiz = this.shadowRoot.querySelector('.quiz')
-    this.btns = this.shadowRoot.querySelector('.btns')
-    this.alt1 = this.shadowRoot.querySelector('#alt1')
-    this.alt2 = this.shadowRoot.querySelector('#alt2')
-    this.alt3 = this.shadowRoot.querySelector('#alt3')
-    this.alt4 = this.shadowRoot.querySelector('#alt4')
+    this.quizQuestion = this.shadowRoot.querySelector('.quiz-question')
     this.pQuestion = this.shadowRoot.querySelector('#question')
+
+    this.btns = this.shadowRoot.querySelector('.btns')
+
     this.answerInput = this.shadowRoot.querySelector('#answerInput')
     this.answerBtn = this.shadowRoot.querySelector('#answerBtn')
+
+    this.responseDiv = this.shadowRoot.querySelector('.response')
+    this.nextQBtn = this.shadowRoot.querySelector('#next-question')
+    this.pAnswer = this.shadowRoot.querySelector('#message-response')
+
+    this.restartDiv = this.shadowRoot.querySelector('.restart-quiz')
+
     this.nextURL = 'http://vhost3.lnu.se:20080/question/1'
   }
 
   connectedCallback () {
+    // this._clearLocalStorage()
     this.startForm.addEventListener('input', event => {
       this._validateForm()
     })
+
     this.startBtn.addEventListener('click', event => {
+      this.username = this.startForm.value
+
       this._hideStartForm()
       this.getQuestion(this.nextURL)
       this.getAnswer()
     })
+  }
+
+  /**
+   * sends the url to servers and gets obj
+   * @param {string} nextURL
+   */
+  async getQuestion (nextURL) {
+    let firstQuestion = await window.fetch(nextURL)
+    firstQuestion = await firstQuestion.json()
+    this.obj = firstQuestion
+    this.nextURL = firstQuestion.nextURL
+
+    this.renderQuestion(this.obj)
+  }
+
+  /**
+   * Renders the question to html
+   * @param {object} obj
+   * @param {string} url
+   */
+  renderQuestion (obj) {
+    this._hideResponse()
+    this.pQuestion.innerText = `${obj.question}`
+    this.quiz.style.display = 'block'
+
+    if (obj.alternatives) {
+      this._hideAnswerInput()
+      this.btns.innerText = ''
+      let i = 1
+      for (const alt in this.obj.alternatives) {
+        const altBtn = document.createElement('button')
+        altBtn.innerText = this.obj.alternatives[alt]
+        altBtn.setAttribute('id', 'alt' + i)
+        this.btns.appendChild(altBtn)
+        i++
+      }
+      this._showQuiz()
+    } else {
+      this.btns.style.display = 'none'
+      this._showAnswerInput()
+    }
+  }
+
+  /**
+   * Checks the values of input and buttons
+   */
+  getAnswer () {
+    this.answerBtn.addEventListener('click', async event => {
+      this.answer = this.answerInput.value
+      await this.postAnswer(this.answer)
+    })
+    this.btns.addEventListener('click', async event => {
+      if (event.target.id === 'alt1' || event.target.id === 'alt2' || event.target.id === 'alt3' || event.target.id === 'alt4') {
+        this.answer = event.target.id
+        await this.postAnswer(this.answer)
+      }
+    })
+  }
+
+  renderAnswer (obj) {
+    if (obj.message.length === 15 && !obj.nextURL) {
+      this.onWin()
+      this._hideQuiz()
+      this._showScoreBoard()
+    } else if (obj.message.length === 16) {
+      this.onLoss()
+      this._hideQuiz()
+      this._showRestart()
+    } else {
+      this._hideQuiz()
+      this.pAnswer.innerText = obj.message
+      this.responseDiv.style.display = 'block'
+      this.nextQBtn.addEventListener('click', event => {
+        this.getQuestion(this.nextURL)
+      })
+    }
+  }
+
+  async postAnswer (answerVal) {
+    const data = { answer: answerVal }
+    const settings = {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    try {
+      const postResponse = await window.fetch(this.nextURL, settings)
+      this.obj = await postResponse.json()
+      this.nextURL = this.obj.nextURL
+      this.renderAnswer(this.obj)
+    } catch (error) {
+      console.log(error)
+      return error
+    }
+  }
+
+  saveToLocalStorage (username) {
+    let players = window.localStorage.getItem('scoreBoard')
+    if (players === null) {
+      let players = []
+      players[0] = { name: username, score: 'time' }
+      players = JSON.stringify(players)
+      window.localStorage.setItem('scoreBoard', players)
+      return JSON.parse(players)
+    } else {
+      players = JSON.parse(players)
+      players.push({ name: username, score: 'time' })
+      this.sortScore(players)
+    }
+    players = JSON.stringify(players)
+    window.localStorage.setItem('scoreBoard', players)
+    return JSON.parse(players)
+  }
+
+  sortScore (players) {
+
+  }
+
+  onWin () {
+    console.log('win')
+    this.players = this.saveToLocalStorage(this.username)
+    for (let i = 0; i < 5; i++) {
+      const li = document.createElement('li')
+      li.innerText = `Username: ${this.players[i].name}. Time: ${this.players[i].score}`
+      this.scoreList.appendChild(li)
+    }
+  }
+
+  onLoss () {
+    console.log('loss')
+  }
+
+  setTimer () {
+
   }
 
   /**
@@ -91,83 +283,49 @@ export class Refactor extends window.HTMLElement {
     }
   }
 
-  /**
-   * sends the url to servers and gets obj
-   * @param {string} nextURL
-   */
-  async getQuestion (nextURL) {
-    let firstQuestion = await window.fetch(nextURL)
-    firstQuestion = await firstQuestion.json()
-    const obj = firstQuestion
-    this.answerURL = firstQuestion.nextURL
-
-    this.renderQuestion(obj)
+  /*
+    Helper methods below to hide or show dom elements
+  */
+  _showAnswerInput () {
+    this.answerInput.value = ''
+    this.quizQuestion.style.display = 'block'
+    this.answerInput.style.display = 'inline'
+    this.answerBtn.style.display = 'inline'
   }
 
-  /**
-   * Renders the question to html
-   * @param {object} obj
-   * @param {string} url
-   */
-  renderQuestion (obj, url) {
-    console.log(obj)
-    this.pQuestion.innerText = `${obj.question}`
+  _showRestart () {
+    this.restartDiv.style.display = 'block'
+  }
+
+  _showQuiz () {
+    this.quizQuestion.style.display = 'block'
     this.quiz.style.display = 'block'
-    if (obj.alternatives) {
-      this.alt1.innerHTML = obj.alternatives.alt1
-      this.alt2.innerHTML = obj.alternatives.alt2
-      this.alt3.innerHTML = obj.alternatives.alt3
-      this.alt4.innerHTML = obj.alternatives.alt4
-      this.quiz.style.display = 'block'
-      this.btns.style.display = 'block'
-    } else {
-      this.answerInput.style.display = 'inline'
-      this.answerBtn.style.display = 'inline'
-    }
+    this.btns.style.display = 'block'
   }
 
-  /**
-   * Checks the values of input and buttons
-   */
-  getAnswer () {
-    this.answerBtn.addEventListener('click', async event => {
-      this.answer = this.answerInput.value
-      await this.postAnswer(this.answer)
-    })
-    this.btns.addEventListener('click', async event => {
-      if (event.target.id === 'alt1' || event.target.id === 'alt2' || event.target.id === 'alt3' || event.target.id === 'alt4') {
-        console.log(event.target.id)
-        this.answer = event.target.id
-        await this.postAnswer(this.answer)
-      }
-    })
+  _showScoreBoard () {
+    this.scoreBoard.style.display = 'block'
   }
 
-  async postAnswer (answerVal) {
-    console.log(answerVal)
-    const data = { answer: answerVal }
-    const settings = {
-      method: 'Post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }
-    try {
-      const postResponse = await window.fetch(this.answerURL, settings)
-      const getData = await postResponse.json()
-      console.log(getData)
-    } catch (error) {
-      console.log(error)
-      return error
-    }
+  _hideAnswerInput () {
+    this.answerInput.style.display = 'none'
+    this.answerBtn.style.display = 'none'
   }
 
-  /**
-   * Helper method to hide div
-   */
+  _hideResponse () {
+    this.responseDiv.style.display = 'none'
+  }
+
+  _hideQuiz () {
+    this.quizQuestion.style.display = 'none'
+  }
+
   _hideStartForm () {
     this.constainerStart.style.display = 'none'
+  }
+
+  _clearLocalStorage () {
+    window.localStorage.clear()
   }
 }
 
