65c9fcf04acf1efbfa3ac3a9ed9f70481408671d
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-03-09 17:33:37 +0300

    Comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
-import { QuizGameRef } from './quiz-ref.js'
-import { QuizGame } from './quiz-component.js'
+/**
+ * Credit to: ”Linnaeus university”
+ *  https://webbprogrammerare.se
+ */
+
+import { QuizGameRef } from './quiz-component-ref.js'
+import { QuizGame } from './quiz-component-old.js'
